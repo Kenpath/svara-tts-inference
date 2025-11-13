789390fed76c2e7e83169cfbf7c69d1795270a5a
Aditya Chhabra <aditya0chhabra@gmail.com>
2025-11-13 22:25:40 +0530

    Copy exact format from notebook for testing

--- tts_engine/utils.py
+++ tts_engine/utils.py
@@ -82,7 +82,7 @@ def svara_zero_shot_prompt(
     # 128261 -> <custom_token_5>, 128257 -> <custom_token_1>, 128258 -> <custom_token_2>,
     # 128262 -> <custom_token_6>, 128266+ -> audio tokens
     audio_token_str = "".join([f"<custom_token_{token}>" for token in audio_tokens])
-    previous_audio_prompt = f"{START_OF_SPEECH}{audio_token_str}{END_OF_SPEECH}"
+    previous_audio_prompt = f"{audio_token_str}{END_OF_SPEECH}{END_OF_AI}"
     previous_text_prompt  = f"{START_OF_HUMAN}{AUDIO} {transcript}{EOT_ID}{END_OF_HUMAN}" if transcript and transcript.strip() else ""
     current_text_prompt   = f"{START_OF_HUMAN}{AUDIO} {text}{EOT_ID}{END_OF_HUMAN}{START_OF_AI}"
     
